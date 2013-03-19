965850ebaf2b41d436b3b7d5d8a7533f5eb3c4de
Henry Eastman <henry.eastman@gmail.com>
2013-03-19 13:18:06 -0400

    Created a Ghost
    You now see where a block will be placed, and you can place with left
    click. I havent tested with the controller yet, but it should be the
    right bumper.

--- Tower Defense/Assets/Player.cs
+++ Tower Defense/Assets/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour {
 	public float xPosition = 0;
 	public float yPosition = 0;
 	
+	public GameObject ghostTower;
+	private bool towerPlaced = false;
+	
 	// Use this for initialization
 	void Start () {
 		Screen.showCursor = false;
@@ -20,5 +23,14 @@ public class Player : MonoBehaviour {
 		this.transform.Translate (new Vector3(xMove,yMove,0));
 		this.xPosition = Mathf.Round(transform.position.x);
 		this.yPosition = Mathf.Round(transform.position.y);
+		ghostTower.transform.position = new Vector3(xPosition,yPosition,0);
+		if(!towerPlaced && Input.GetAxis("Place Tower") >= .5){
+			towerPlaced = true;
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.transform.position = new Vector3(xPosition,yPosition,0);
+		}
+		if(towerPlaced && Input.GetAxis("Place Tower") <= .5){
+			towerPlaced = false;	
+		}
 	}
 }
