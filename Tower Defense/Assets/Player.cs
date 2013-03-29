7e9a6eac6bb44c0322b87df26e0448197dce94c8
Henry Eastman <henry.eastman@gmail.com>
2013-03-29 11:54:38 -0400

    Added new inputs.
    Nudging and zoom will set up.

--- Tower Defense/Assets/Player.cs
+++ Tower Defense/Assets/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour {
 	public float moveSpeed = 1;
 	
 	public int xPosition = 0;
-	public int yPosition = 0;
+	public int zPosition = 0;
 	
 	public GameObject ghostTower;
 	public GameObject towerToPlace;
@@ -22,11 +22,11 @@ public class Player : MonoBehaviour {
 	// Fixed Update is called once per physics frame
 	void FixedUpdate () {
 		float xMove = moveSpeed * Input.GetAxis("Horizontal");
-		float yMove = moveSpeed * Input.GetAxis("Vertical");
+		float zMove = moveSpeed * Input.GetAxis("Vertical");
 		this.transform.Translate (new Vector3(xMove,yMove,0));
 		this.xPosition = (int) Mathf.Round(transform.position.x);
-		this.yPosition = (int) Mathf.Round(transform.position.y);
-		ghostTower.transform.position = new Vector3(xPosition,yPosition,0);
+		this.zPosition = (int) Mathf.Round(transform.position.z);
+		ghostTower.transform.position = new Vector3(xPosition,0,zMove);
 		if(!towerPlaced && Input.GetAxis("Place Tower") >= .5){
 			towerPlaced = gm.placeTower(xPosition,yPosition,towerToPlace);
 		}
